4a1c12ea8d17a357927a4e9a76a618c38329fcd8
JekaKataev <pro.jeka.pro@gmail.com>
2024-01-31 08:34:24 +0200

    add comment

--- js/script8.js
+++ js/script8.js
@@ -1,14 +1,14 @@
 let num = parseInt(prompt("шестизначное число"));
 
-let num_end = num % 1000;
-let num_begin = (num - num_end) / 1000;
+let num_end = num % 1000; //last 3 digits of the number
+let num_first = (num - num_end) / 1000; //first 3 digits of the number
 
-let num_begin_three = num_begin % 10;
-let num_begin_two = ((num_begin % 100) - num_begin_three) / 10;
-let num_begin_one = (num_begin - num_begin_two * 10 - num_begin_three) / 100
+let num_first_three = num_first % 10;
+let num_first_two = ((num_first % 100) - num_first_three) / 10;
+let num_first_one = (num_first - num_first_two * 10 - num_first_three) / 100
 
-let num_beginMirror = String(num_begin_three) + String(num_begin_two) + String(num_begin_one);
+let num_firstMirror = String(num_first_three) + String(num_first_two) + String(num_first_one);
 
-let result = (Number(num_beginMirror) === num_end) ? "число зеркальное" : "число не зеркальное";
+let result = (Number(num_firstMirror) === num_end) ? "число зеркальное" : "число не зеркальное";
 
 alert(result);
