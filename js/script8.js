880ea6b89b1d13d315929c7a4675e38398cb0757
JekaKataev <pro.jeka.pro@gmail.com>
2024-02-04 19:21:05 +0200

    fix2

--- js/script8.js
+++ js/script8.js
@@ -2,7 +2,7 @@ let result = '';
 let a = 100;
 let b = 200;
 
-for (a; a <= 200; a++){
+for (a; a <= b; a++){
     result += (a % 3 === 0) ? `${a + 3 >= b ? `${a}` : `${a}, `}`: ''; // a + 3 checking the last value to remove the comma at the end
 }
 alert(result);
