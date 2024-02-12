bd6b555701dc060ec75e23c2e65942a721516452
JekaKataev <pro.jeka.pro@gmail.com>
2024-02-12 17:48:52 +0200

    init example_11

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,75 @@
-let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let sumElem = 0;
-let quantity = 0;
+let users = [
+    {
+        "index": 0,
+        "isActive": true,
+        "balance": "$2,226.60",
+        "name": "Eugenia Sawyer",
+        "gender": "female",
+        "phone": "+1 (840) 583-3207",
+        "address": "949 John Street, Rose, Puerto Rico, 1857"
+    },
+    {
+        "index": 1,
+        "isActive": true,
+        "balance": "$2,613.77",
+        "name": "Pauline Gallegos",
+        "gender": "female",
+        "phone": "+1 (985) 593-3328",
+        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+    },
+    {
+        "index": 2,
+        "isActive": false,
+        "balance": "$3,976.41",
+        "name": "Middleton Chaney",
+        "gender": "male",
+        "phone": "+1 (995) 591-2478",
+        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+    },
+    {
+        "index": 3,
+        "isActive": true,
+        "balance": "$1,934.58",
+        "name": "Burns Poole",
+        "gender": "male",
+        "phone": "+1 (885) 559-3422",
+        "address": "730 Seba Avenue, Osage, Alabama, 6290"
+    },
+    {
+        "index": 4,
+        "isActive": true,
+        "balance": "$3,261.65",
+        "name": "Mcfadden Horne",
+        "gender": "male",
+        "phone": "+1 (942) 565-3988",
+        "address": "120 Scholes Street, Kirk, Michigan, 1018"
+    },
+    {
+        "index": 5,
+        "isActive": false,
+        "balance": "$1,790.56",
+        "name": "Suzette Lewis",
+        "gender": "female",
+        "phone": "+1 (837) 586-3283",
+        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+    }
+];
 
-for (let a = 0; a < arr.length; a++) {
-    let num = parseFloat(arr[a]);
-     if (num > 0) {
-         sumElem += num;
-         quantity += 1;
-     }
+let resultTel = [];
+let resultSumBal = 0;
+
+for (let a = 0; a < users.length; a++) {
+    let balance = users[a].balance;
+    let numString = balance.slice(1);
+    let numStringNoComa = "";
+        for (let i = 0; i < numString.length; i++){
+            numStringNoComa += (numString[i] !== ",") ? numString[i] : "";
+        }
+    let balanceNum = parseFloat(numStringNoComa);
+    if (balanceNum > 2000) {
+        resultTel.push(users[a].phone);
+    }
+    resultSumBal += balanceNum;
 }
-alert(`Cумма положительный эллементов = ${sumElem}\nКоличество эллементов = ${quantity}`);
-
+console.log(resultTel);
+console.log(`сумма баланса пользователей = ${resultSumBal}`);
