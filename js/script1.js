4ecbba3351e6cab343b7a847defa1e835bc200b8
JekaKataev <pro.jeka.pro@gmail.com>
2024-03-04 17:32:10 +0200

    init example_16

--- js/script1.js
+++ js/script1.js
@@ -1,17 +1,18 @@
-let ladder = {
-    step: 0,
-    up() {
-        this.step++;
-        return this;
-    },
-    down() {
-        this.step--;
-        return this;
-    },
-    showStep() {
-        alert(this.step);
-        return this
-    }
-};
+function generateKey(length, characters) {
+    let resultArr = [];
+    for (let i = 0; i < length; i++) {
+            resultArr.push(characters[Math.floor(getRandomIntInclusive(characters.length))]);
+        }
+    resultArr = resultArr.join("");
+    return resultArr;
+}
+function getRandomIntInclusive(max) {
+    let min = 0;
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-ladder.up().up().showStep().down().up().showStep();
+const characters = 'gfjd2fh4';
+
+const key = generateKey(10, characters);
+console.log(key);
