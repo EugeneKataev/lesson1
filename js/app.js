0ed75fc0164e279bae90e50ee9474bbcf5b444b9
JekaKataev <pro.jeka.pro@gmail.com>
2024-03-13 22:26:17 +0200

    init example_20

---
+++ js/app.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', showCategories);
+
+document.getElementById('categories').addEventListener('click', event => {
+  const categoryId = event.target.getAttribute('data-category');
+  showProductsByCategory(categoryId);
+});
+
+document.getElementById('products').addEventListener('click', event => {
+  const productId = event.target.getAttribute('data-product');
+  const categoryId = event.target.getAttribute('data-category');
+  
+  showProductInfo(categoryId, productId);
+});
+document.getElementById('product').addEventListener('click', event => {
+  if (event.target.classList.contains('btnSubmit')) {
+    let productName = document.querySelector('.productName');
+    let productPrice = document.querySelector('.productPrice');
+    let infoBlock = document.getElementById('info');
+    infoBlock.innerHTML = `
+    <h1>Order for ${productName.textContent}</h1>
+    <h1>${productPrice.textContent}</h1>
+    <h1 style="color:green">Accepted</h1>
+    `;
+    setTimeout(resetInputs, 1500);
+  }
+});
--- js/script1.js
+++
@@ -1,5 +0,0 @@
-let randomNum = Math.floor(Math.random() * 9) + 1;
-
-let img = document.createElement("img");
-img.src = `images/${randomNum}.jpg`
-document.body.appendChild(img);
