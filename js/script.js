01c1b990114f9d99fedbdc94a03f5b920d228e60
JekaKataev <pro.jeka.pro@gmail.com>
2024-03-18 16:06:03 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ text += prompt("вторая строка");
 text += prompt("третья строка");
     alert(text);
 
-let Number = prompt("5ти значное число");
-let fiveNumber = Number % 10;
-let fourNumber = (Number % 100 - fiveNumber) / 10;
-let threeNumber = (Number % 1000 - Number % 100) / 100;
-let twoNumber = (Number % 10000 - Number % 1000) / 1000;
-let oneNumber = (Number - Number % 10000) / 10000;
+let num = prompt("5ти значное число");
+let fiveNumber = num % 10;
+let fourNumber = (num % 100 - fiveNumber) / 10;
+let threeNumber = (num % 1000 - num % 100) / 100;
+let twoNumber = (num % 10000 - num % 1000) / 1000;
+let oneNumber = (num - num % 10000) / 10000;
     alert(`${oneNumber} ${twoNumber} ${threeNumber} ${fourNumber} ${fiveNumber}`); //from the example in the first lesson
