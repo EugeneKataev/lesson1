1c997cf76f6116b98be004326388ee5b1524486e
JekaKataev <pro.jeka.pro@gmail.com>
2024-03-20 13:32:04 +0200

    init example_25

--- js/data.js
+++ js/data.js
@@ -2,6 +2,9 @@ const users = [
   {
     id: "1",
     name: 'John Smith',
+    password: "qwerty123",
+    phone: "+38099911188",
+    card: "5457984568294371",
     login: 'jsmith',
     email: 'john.smith@ithillel.ua',
     age: 30,
@@ -9,6 +12,9 @@ const users = [
   {
     id: "2",
     name: 'Stacie Mcdonald',
+    password: "zFa4yf",
+    phone: "+380856565656",
+    card: "5457764068292499",
     login: 'smcdonald',
     email: 'stacie.mcdonald@ithillel.ua',
     age: 25,
--- js/functions.js
+++ js/functions.js
@@ -6,10 +6,12 @@ function createElement(typeName, classArr, text) {
   elem.innerHTML = text;
   return elem
 }
+
 function getUsersAndCheckLocalStorage() {
   let localStorageUsers = localStorage.getItem("dataUsers");
   return localStorageUsers !== null ? JSON.parse(localStorageUsers) : users;
 }
+
 function showUsers() {
   let usersBlock = document.getElementById("users");
   usersBlock.innerHTML = "";
@@ -30,6 +32,7 @@ function showUsers() {
     userBtnBlock.appendChild(btnDelete);
   })
 }
+
 function panelEditInfoAdd(option) {
   let editBlock = document.getElementById("panel-add-edit");
   let h2Element = document.querySelector(".head-block-edit h2");
@@ -58,6 +61,7 @@ function panelEditInfoAdd(option) {
       blockInfo.innerHTML = "";
   }
 }
+
 function showUserViewInfo(elem) {
   const userId = elem.target.parentNode.getAttribute("data-id");
   let userData = getUsersAndCheckLocalStorage();
@@ -68,24 +72,35 @@ function showUserViewInfo(elem) {
   blockInfo.innerHTML = `
   <div class="block-edit"><p> name: </p><p>${objUser.name}</p></div>
   <div class="block-edit"><p> login: </p><p>${objUser.login}</p></div>
+  <div class="block-edit"><p> password: </p><p>${objUser.password}</p></div>
+  <div class="block-edit"><p> phone: </p><p>${objUser.phone}</p></div>
+  <div class="block-edit"><p> bank card: </p><p>${objUser.card}</p></div>
   <div class="block-edit"><p> email: </p><p>${objUser.email}</p></div>
   <div class="block-edit"><p> age: </p><p>${objUser.age}</p></div>
   `;
 }
+
 function editUser(elem) {
   const userId = elem.target.parentNode.getAttribute("data-id");
   let userData = getUsersAndCheckLocalStorage();
   const objUser = userData.find(obj => obj.id === userId);
   let inputName = document.getElementById("input-name");
   let inputLogin = document.getElementById("input-login");
+  let inputPassword = document.getElementById("input-password");
+  let inputPhone = document.getElementById("input-phone");
+  let inputCard = document.getElementById("input-card");
   let inputEmail = document.getElementById("input-email");
   let inputAge = document.getElementById("input-age");
   inputName.value = objUser.name;
   inputLogin.value = objUser.login;
+  inputPassword.value = objUser.password;
+  inputPhone.value = objUser.phone;
+  inputCard.value = objUser.card;
   inputEmail.value = objUser.email;
   inputAge.value = objUser.age;
   document.getElementById("btnSave").setAttribute("data-id", userId);
 }
+
 function validationInputs(arrElem) {
   let valid = true;
   arrElem.forEach((elem)=>{
@@ -95,16 +110,54 @@ function validationInputs(arrElem) {
     } else {
       elem.classList.remove("error");
     }
+    switch (elem.id) {
+      case "input-name":
+        valid = !validRegular(elem, /^[A-Za-zА-Яа-я]{2,}/) ? false : valid;
+        break;
+      case "input-login":
+        valid = !validRegular(elem, /^[a-zA-Z0-9_-]{4,}$/) ? false : valid;
+        break;
+      case "input-password":
+        valid = !validRegular(elem, /^[a-zA-Z\S]+$/) ? false : valid;
+        break;
+      case "input-phone":
+        valid = !validRegular(elem, /^[\d\s\-\+\(\)]+$/) ? false : valid; // allowed numbers + - space ( )
+        break;
+      case "input-card":
+       valid = !validRegular(elem, /^(\d{4}\s\d{4}\s\d{4}\s\d{4}|\d{16})$/) ? false : valid;
+        break;
+      case "input-email":
+        valid = !validRegular(elem, /^[A-Za-z0-9_\-.]{2,}@([A-Za-z0-9]+\.)+[A-Za-z]{1,}$/) ? false : valid;
+        break;
+      case "input-age":
+        valid = !validRegular(elem, /^[1-9][0-9]*$/) ? false : valid;
+        break;
+    }
   })
   return valid;
 }
+
+function validRegular(elem, reg) {
+  let value = elem.value;
+  if (!value.match(reg)) {
+    elem.classList.add("error");
+    return false
+  } else {
+    elem.classList.remove("error");
+    return true
+  }
+}
+
 function userSave(elem) {
   let userId = elem.target.getAttribute("data-id");
   let name = document.getElementById("input-name");
   let login = document.getElementById("input-login");
+  let password = document.getElementById("input-password");
+  let phone = document.getElementById("input-phone");
+  let card = document.getElementById("input-card");
   let email = document.getElementById("input-email");
   let age = document.getElementById("input-age");
-  let arrCheckValid = [name, login, email, age];
+  let arrCheckValid = [name, login, password, phone, card, email, age];
 
   let validation = validationInputs(arrCheckValid);
 
@@ -116,6 +169,9 @@ function userSave(elem) {
         id: newId,
         name: name.value,
         login: login.value,
+        password: password.value,
+        phone: phone.value,
+        card: card.value,
         email: email.value,
         age: age.value,
       }
@@ -126,6 +182,9 @@ function userSave(elem) {
         if (obj.id === userId) {
           obj.name = name.value;
           obj.login = login.value;
+          obj.password = password.value;
+          obj.phone = phone.value;
+          obj.card = card.value;
           obj.email = email.value;
           obj.age = Number(age.value);
         }
@@ -138,10 +197,9 @@ function userSave(elem) {
 }
 
 function createUser() {
-  document.getElementById("input-name").value = "";
-  document.getElementById("input-login").value = "";
-  document.getElementById("input-email").value = "";
-  document.getElementById("input-age").value = "";
+  ["input-name", "input-login", "input-password", "input-phone", "input-card", "input-email", "input-age"].forEach(id =>{
+    document.getElementById(id).value = "";
+  })
   let btnCreate = document.getElementById("btnSave");
   btnCreate.setAttribute("data-id", "");
 }
