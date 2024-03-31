285ae35eb412de0b60bab79fb07c7e6497405d1a
JekaKataev <pro.jeka.pro@gmail.com>
2024-03-31 21:56:35 +0300

    init example_26

--- js/app.js
+++ js/app.js
@@ -1,24 +1,51 @@
-window.addEventListener('DOMContentLoaded', showUsers);
+const modal = document.getElementById("myModal");
+const btnModal = document.getElementById("openModal");
+const btnAlert = document.getElementById("openAlert");
+const span = document.getElementsByClassName("close")[0];
+const tooltip = document.getElementById("info");
+const AlertBlock = document.getElementById("headAlert");
+const inputDate = document.getElementById("form_date");
+const formInfoDate = document.querySelector(".form-info-date");
 
-document.getElementById("users").addEventListener("click", (e)=>{
-    switch (true) {
-        case e.target.classList.contains("btn-view"):
-            panelEditInfoAdd("view");
-            showUserViewInfo(e);
-                break;
-        case e.target.classList.contains("btn-edit"):
-            panelEditInfoAdd("edit");
-            editUser(e);
-                break;
-        case e.target.classList.contains("btn-del"):
-            deleteUser(e);
-                break;
+btnModal.addEventListener("click", () => {
+    modal.classList.add("active");
+});
+btnModal.addEventListener("mouseover", (e) => {
+    tooltip.classList.add("active");
+    let btnWidth = e.target.offsetWidth;
+    tooltip.style.left = `${btnWidth + 4}px`
+    tooltip.style.top = `-17px`
+});
+btnModal.addEventListener("mouseout", () => {
+    tooltip.classList.remove("active");
+});
+
+btnAlert.addEventListener("click", () => {
+    AlertBlock.classList.toggle("active");
+})
+
+span.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+        modal.classList.remove("active");
     }
-})
-document.getElementById("btnSave").addEventListener("click", (e)=>{
-    userSave(e); //create and edit
-})
-document.getElementById("add-user").addEventListener("click", (e)=>{
-    panelEditInfoAdd("add");
-    createUser();
+});
+inputDate.addEventListener("blur", (e) => {
+    let valInput = e.target.value;
+    const reg = /^(0[1-9]|[1-2][0-9]|3[0-1])\.(0[1-9]|1[0-2])\.\d{4}$/;
+
+    if (!reg.test(valInput)) {
+        e.target.classList.add("is-invalid");
+        formInfoDate.innerHTML = "";
+    } else {
+        e.target.classList.remove("is-invalid");
+        let infoElem = document.createElement("p");
+        let convertDate = moment(valInput, "DD-MM-YYYY").locale("ru").format("DD MMMM YYYY г.");
+        infoElem.textContent = `Ваша дата рождения: ${convertDate}`;
+        formInfoDate.innerHTML = "";
+        formInfoDate.appendChild(infoElem);
+    }
 })
