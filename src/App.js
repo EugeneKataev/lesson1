65331e1d442fc12bbb697c5cb18e7ef94e0c226f
JekaKataev <pro.jeka.pro@gmail.com>
2024-06-06 18:17:12 +0300

    init example_34

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom";
+import Contacts from "./Components/Contacts";
+import AddContact from "./Components/AddContact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+      <div className="head-menu">
+        <ul>
+          <li><NavLink to="/">Contacts</NavLink></li>
+          <li><NavLink to="/add">Add Contact</NavLink></li>
+        </ul>
+      </div>
+        <Routes>
+          <Route exact path="/" element={<Contacts />} />
+          <Route path="/add" element={<AddContact />} />
+          <Route path="/edit/:id" element={<AddContact />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
